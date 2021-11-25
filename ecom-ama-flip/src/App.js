ba00e7bb0439fd5faeb9a8df94ea24b815fac708
Ganesh Kollati <52064346+Ganeshkollatii98@users.noreply.github.com>
2021-11-25 05:37:27 +0530

    share code on git hub

--- ecom-ama-flip/src/App.js
+++ ecom-ama-flip/src/App.js
@@ -1,10 +1,11 @@
 
 import './index.css';
+import { HomePage } from './Pages/HomePage';
 function App() {
   return (
     <div className="App bg-red-500">
-    app
-          </div>
+    <HomePage/>
+    </div>
   );
 }
 
---
+++ ecom-ama-flip/src/Pages/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <div>
+                <a href="#"> ecommerce</a>
+            </div>
+            <div>
+                <a href="#">Cart</a>
+                <a href="#">Sign In</a>
+            </div>
+        </header>
+    )
+}
