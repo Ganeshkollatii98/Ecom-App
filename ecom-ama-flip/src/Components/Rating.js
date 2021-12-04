8537ef9b935b31e4e19ab6efd151914468481782
Ganesh Kollati <52064346+Ganeshkollatii98@users.noreply.github.com>
2021-12-04 06:08:37 +0530

    Rating are displaying based json data(data.js)

--- ecom-ama-flip/src/Components/Rating.js
+++ ecom-ama-flip/src/Components/Rating.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import StarIcon from "@mui/icons-material/Star";
 import StarHalfIcon from "@mui/icons-material/StarHalf";
-
+import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
 export default function Rating(props) {
-    const {rating,numReviews} =props;
+    const {rating,numReviews} =props.product;
     return (
         <div className="flex cursor-pointer pl-2 mt-3 pb-3  product-ratings ">
         <div className="text-yellow-500 pr-1">
-        {rating}
-        <StarIcon style={{ fontSize: "1.3rem" }} />
+         
+         {rating>=1 ? <StarIcon  style={{ fontSize: "1.3rem" }} /> : rating>=0.5 ?<StarHalfIcon style={{ fontSize: "1.3rem" }} />:<StarOutlineOutlinedIcon style={{ fontSize: "1.3rem" }} />}
+         {rating>=2 ? <StarIcon style={{ fontSize: "1.3rem" }} /> : rating>=1.5 ?<StarHalfIcon style={{ fontSize: "1.3rem" }} />:<StarOutlineOutlinedIcon style={{ fontSize: "1.3rem" }} />}
+         {rating>=3 ? <StarIcon style={{ fontSize: "1.3rem" }} /> : rating>=2.5 ?<StarHalfIcon style={{ fontSize: "1.3rem" }} />:<StarOutlineOutlinedIcon style={{ fontSize: "1.3rem" }} />}
+         {rating>=4 ? <StarIcon style={{ fontSize: "1.3rem" }} /> : rating>=3.5 ?<StarHalfIcon style={{ fontSize: "1.3rem" }} />:<StarOutlineOutlinedIcon style={{ fontSize: "1.3rem" }} />}
+         {rating>=5 ? <StarIcon style={{ fontSize: "1.3rem" }} /> : rating>=4.5 ?<StarHalfIcon style={{ fontSize: "1.3rem" }} />:<StarOutlineOutlinedIcon style={{ fontSize: "1.3rem" }} />}
+
+
+        {/* <StarIcon style={{ fontSize: "1.3rem" }} />
         <StarIcon style={{ fontSize: "1.3rem" }} />
         <StarIcon style={{ fontSize: "1.3rem" }} />
         <StarIcon style={{ fontSize: "1.3rem" }} />
         <StarHalfIcon style={{ fontSize: "1.3rem" }} />
+         <StarOutlineOutlinedIcon style={{ fontSize: "1.3rem" }} /> */}
         </div>
-        <a href="#"><span className="text-gray-500 hover:text-gray-900">{numReviews} Reviews</span></a>
+        <a href="#"><span className=" hover:text-gray-900">{numReviews} Reviews</span></a>
       </div>
     )
 }
