f86bc64365a513b43b3febea2d19b7baab5481d7
Ganesh Kollati <52064346+Ganeshkollatii98@users.noreply.github.com>
2021-12-07 17:34:10 +0530

    loading box and message box are updated

--- ecom-ama-flip/frontend/src/Screens/HomeScreen.js
+++ ecom-ama-flip/frontend/src/Screens/HomeScreen.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Product from '../Components/Product'
 import data from "../data";
+import LoadingBox from '../Components/LoadingBox';
+import MessageBox from '../Components/MessageBox'
 export default function HomeScreen() {
+    const [products,setProducts]=useState([]);
+    const [loading,setLoading]=useState(false);
+    const [error,setError]=useState()
+     useEffect(() => {
+        const featchData= async()=>{
+            try{
+                setLoading(true);
+                const {data}=await axios.get('/api/products')
+                setLoading(false);
+                setProducts(data);
+            }
+            catch(error){
+               setError(error.message);
+               setLoading(false);
+            }
+
+        } 
+        featchData();
+       
+     }, [])
+
+
     return (
-        <div class="products-container pb-16  w-full flex flex-wrap  justify-around">
-        {data.products.map((item)=>{      
+        <div >
+        {loading ? (<LoadingBox/>) : error ?( <MessageBox err={error}/>) : 
+          (<div class="products-container pb-16  w-full flex flex-wrap  justify-around">
+            {products.map((item)=>{      
            return <Product product={item} key={item._id} />
         })}
+          </div>)
+          
+        }
+    
        </div>
     )
 }
